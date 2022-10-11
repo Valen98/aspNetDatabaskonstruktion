bd2a2ce8327b67e5514c28127291f56a3e5d9cde
Valen98 <leo.wahlandtcs@gmail.com>
2022-10-11 15:39:00 +0200

    First initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using B21leowa_DOTNet.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace B21leowa_DOTNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
