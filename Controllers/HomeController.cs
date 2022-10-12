8d9201df70eaa6c2387cc9ebf0fc8a37c41d1048
Valen98 <leo.wahlandtcs@gmail.com>
2022-10-12 18:35:43 +0200

    Added a form for insert into child but it does not work... Created GetAllChildren method and displays it on index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,20 +7,35 @@ namespace B21leowa_DOTNet.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private IConfiguration _configuration;
+        private BarnModel _barnModel;
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
+            _configuration = configuration;
             _logger = logger;
+            _barnModel = new BarnModel(_configuration);
         }
 
         public IActionResult Index()
         {
+            ViewBag.barnTable = _barnModel.GetAllChildren();
             return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult CreateChildView()
+        {
+            return View();
+        }
+
+        public IActionResult InsertChild(string PNR, string firstname, string surname, string birthday, int kindnessScale, string pwd)
+        {
+            _barnModel.InsertChild(PNR, firstname, surname, birthday, kindnessScale, pwd);
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/BarnModel.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace B21leowa_DOTNet.Models
+{
+    public class BarnModel
+    {
+        private IConfiguration _configuration;
+        private string _connectionString;
+        public BarnModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration["ConnectionString"];
+        }
+
+        public DataTable GetAllChildren()
+        {
+            MySqlConnection connection = new MySqlConnection(_connectionString);
+            connection.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM barn;", connection);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "result");
+            DataTable barnTable = ds.Tables["result"];
+            connection.Close();
+            return barnTable != null ? barnTable : new DataTable();
+        }
+
+        public void InsertChild(string PNR, string firstname, string surname, string birthday, int kindnessScale, string pwd)
+        {
+            string fullname = String.Concat(firstname, surname);
+            DateTime birthdayDate = Convert.ToDateTime(birthday);
+
+            MySqlConnection connection = new MySqlConnection(_connectionString);
+            connection.Open();
+           
+            Console.WriteLine("THIS IS BEFORE SQL INSERT " + pwd);
+
+            Console.WriteLine("THIS IS PASSWORD ");
+            string insertChild = "INSERT INTO barn(PNR, namn, födelseår, snällhetsSkala, pwd) VALUES (@PNR, @name, @birthday, @kindnessScale, @pwd);";
+            MySqlCommand sqlCmd = new MySqlCommand(insertChild, connection);
+            sqlCmd.Parameters.AddWithValue("@PNR", PNR);
+            sqlCmd.Parameters.AddWithValue("@name", fullname);
+            sqlCmd.Parameters.AddWithValue("@birthday", birthdayDate);
+            sqlCmd.Parameters.AddWithValue("@kindnessScale", kindnessScale);
+            sqlCmd.Parameters.AddWithValue("@pwd", pwd);
+            int rows = sqlCmd.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+}
